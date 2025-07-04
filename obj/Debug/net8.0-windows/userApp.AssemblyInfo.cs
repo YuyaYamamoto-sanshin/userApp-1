8709bdd18971eba737513917e9a3547232dfc845
SANSHIN\yamamoto_yuya <yu_yamamoto@sanshinkinzoku.co.jp>
2025-07-04 17:41:55 +0900

    フォルダ移動

--- obj/Debug/net8.0-windows/userApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/userApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("userApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efab84d266bb07760a07f2d54abe52d94c37078d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9cddaf72faa359df40b621435274fc9d1b8406c")]
 [assembly: System.Reflection.AssemblyProductAttribute("userApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("userApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
